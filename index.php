5c6699958e71211b03507781ba9a4ba173f315c6
Terry Sutton <saltcod@gmail.com>
2013-12-02 15:03:57 -0330

    First commit

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width" />
+	<title>How to take better photos</title>
+
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+	<link href='style.css' rel='stylesheet' type='text/css'>
+</head>
+
+
+
+
+<section class="center first greenbg group">
+	<div class="limiter">
+		<p>From WaterstreetGM</p>
+		<h1 class="site-title">How to take better photos</h1>
+		
+		<div class="table-of-contents">
+			<h3>[ Contents ]</h3>
+			<ol>
+				<li>Jargon & All you Need</li>
+				<li>The Secret</li>
+				<li>One Good Lens</li>
+				<li>Darker Corners</li>
+			</ol>
+		</div>
+
+		<p class="reading-time">Reading time: Approx. 8 minutes </p>
+	</div>
+</section>
+
+<section class="green gray group">
+	<div class="limiter">
+
+		<div class="col2">
+			<div class="section-number"> <span class="number">01</span></div>
+			<h3 class="section-title">Jargon</h3>		 
+		</div>
+
+
+		
+		<div class="col8 margin1 pad2x">
+			<p>There's jargon everywhere you turn. The internet is crammed with it to the point that nothing makes sense anymore, and the sales people at the local camera store are intimidating.</p>
+			<p>The primary goal of this is to help you wade through all the sales-speak and technical nonsense that you don't care about, and focus on taking nice photos of your kids or your cats or whatever you're taking photos of.</p>
+
+			<p>F-stops, shutterspeed, ISO, and all those other settings on your camera are important &#8212; I'm not suggesting that you not use any of these things&#8212;but I am telling you that none of it needs to be complicated. </p>
+			
+			<blockquote class="huge">It's not rocket science</blockquote>
+
+			<p> If you're taking photos now and find them boring or uninspiring, or just want to get closer to some of the great photos you see online, there are a couple of dead-simple techniques you can use to get there.</p>
+
+			<p><strong>Fair warning:</strong> all that follows is my own opinion, gained from years of shooting. Other photographers will have very different opinions about what you should know or what is most important.Your uncle might think that the ISO is important (and he's right!), or your sister might try and convince you that shutter speed is most important (and she's right too!), but I strongly feel that there's one single secret that separates a great photo from an uninspired one. In fact, it's the subject of the next section. </p>
+
+			<p>Let's get started, shall we?</p>
+
+		</div>
+
+	</div>
+</section>
+
+
+<section class="gray group">
+	<div class="limiter">
+
+		<div class="col2">
+			<div class="section-number"> <span class="number">02</span></div>
+			<h3 class="section-title">The Secret</h3>		 
+		</div>
+
+
+		
+		<div class="col8 margin1 pad2x">
+			<p>That's right. <em>The</em> secret.There's just one. The enormous and complicated field of photography is all about one, single secret. Chances are, if you're still unhappy about the photos you're taking with your brand new camera, you haven't discovered this secret yet. You've no doubt heard the word, but maybe no one has impressed on you how important it is. Today's your big day!</p>
+
+			<p><em>And the secret is:</em></p>
+			<p><em>Drumroll</em></p>
+			
+			<h2>Aperture</h2>
+			
+			<p>The one and only thing separating you from better photos of your kids and cats is aperture. That's it. Although your camera has a pile of other settings, <em>this</em> is the one that you should focus your attention on.</p>
+
+
+			<figure>
+				<img src="images/aperturedial.jpg" alt="Aperture Dials from 8 cameras">
+				<figcaption><strong>Figure:</strong> Most camera makers do it differently, but they all have the magic A. Via  <a href="http://katiatrudeau.com/blog/2011/09/qa-photo-101-aperture/">Katia Trudeau</a></figcaption>
+			</figure>
+
+			<h2>What is Aperture?</h2>
+			I said up front that this isn't a technical article, and I'm sticking to it! I don't want to get bogged down in the technical aspects of what aperture is &#8212; you can read all about that <a href="http://en.wikipedia.org/wiki/Aperture">somewhere else</a> if you're interesed. I want to focus on what impact aperture has on your photos. And again, it's my opinion that you don't need to know the technical stuff going on the background to see what aperture can do for you.
+
+			<h2>An Aperture exercise</h2>
+			<p>I want you to forget about the word aperture for a second. Instead, follow along here with your finger:</p>
+			<ol>
+				<li>Hold your finger about one foot from your nose.</li>
+				<li>Stare at your finger.</li>
+				<li>Next, stare at the floor, or a distant wall or window</li>
+				<li>Keep alternating focus between your finger and the wall.</li>
+				<li>Finger</li>
+				<li>Wall</li>
+				<li>Finger</li>
+				<li>Wall</li>
+			</ol>
+			
+			<p>While you're altertaning between focusing on your finger and the wall, just have a look at what happens to the item that you're not focused on. What happens to the wall when you're staring at the finger? It becomes blurry, doesn't it? As a result, your finger becomes more prominent in the scene </p>
+
+
+			<h2>What can aperture do for me?</h2>
+			Aperture is what transforms a normal photo into a great one. When you're shooting photos of people, it's always, always, always going to be aperture that will separate good photos from boring ones. 
+
+		</div>
+
+	</div>
+</section>
+
+
+<section class="gray group">
+	<div class="limiter">
+
+		<div class="col2">
+			<div class="section-number"> <span class="number">03</span></div>
+			<h3 class="section-title">One Good Lens</h3>		 
+		</div>
+
+
+		
+		<div class="col8 margin1 pad2x">
+			<p>Ok. So now we know all about The Secret. You're ready to take some amazing photos of the next party or concert or </p>
+		</div>
+
+	</div>
+</section>
+
+
+
+<section class="gray group">
+	<div class="limiter">
+
+		<div class="col2">
+			<div class="section-number"> <span class="number">04</span></div>
+			<h3 class="section-title">Darken those corners, son</h3>		 
+		</div>
+
+
+		
+		<div class="col8 margin1 pad2x">
+			<p>We've got one main plan here at ParachutePress. We want to help you sleep better at night. </p>
+			<p>When your site is secure, up to date and backing up to an off-site location every day, you'll sleep like a baby. We promise.  </p>
+		</div>
+
+	</div>
+</section>
+
+
+
+
+
+
+
+
+<section class="why-bother red group">
+	<div class="limiter">
+		<h2 class="center"> Why Bother</h2>
+		<h4 class="center callout-small">All of your hard work could be wiped out in a second. We can make sure everything is safe and sound.</h4>
+
+		<div class="group pad4y keyline-bottom">
+			<div class="col5 call">Are you just trying to scare me?</div>
+			<div class="col6 margin0">Absolutely. If you care at all about the hard work you put into your site, you need to have it up to date and backed up. Hours and hours of your hard work could be wiped out in a second.</div>
+		</div>
+
+		<div class="group pad4y keyline-bottom">
+			<div class="col5 call">Can't I just do this myself?</div>
+			<div class="col6 margin0">Definitely. You can do everything we're going to do on you own. You can use the same process completely. We're here for people who aren't comfortable doing that. Or poeple who haven't updated in a while are nervous about something going wrong.</div>
+		</div>
+
+		<div class="group pad4y keyline-bottom">
+			<div class="col5 call">What if my site is a mess?</div>
+			<div class="col6 margin0">If your site is in complete disarray, we can definitely still help with that. It might not be all covered in the $99 PLAN, but we can give you a fair estimate of how long it will take us to get everything fixed. We don't nickel and dime.  </div>
+		</div>
+
+		<div class="group pad4y keyline-bottom">
+			<div class="col5 call">Can I trust you?</div>
+			<div class="col6 margin0">We've done this about 1000 times. Moving sites, updating, setting up backups, cleaning up messes, speeding things up &#8212; we've done it all. We'll take great care with your site &#8212; we know the hard work that goes into them. </div>
+		</div>
+
+
+	</div>
+</section>
+
+
+
+
+
+<section  class="center blue gray">
+	<div class="limiter ">
+		<h2>Ready</h2>
+		<div class="callout-small">The sooner you get your site safely backed up, the better you'll sleep. And you need your sleep.</div> 
+
+		<div class="col12 pad4y"> <a href="#" class="button blue">Get started<i class="icon-chevron-right"></i></a></div>
+	</div>
+
+
+
+</section>
+
+
+
+<section  class="center blue">
+	<div class="limiter">
+		<h2>Bigger issues?</h2>
+		<div class="callout margin4y">Got a disaster brewing?</div> 
+
+		<div class="callout-small margin4y">We also offer a range of helpful services if you're looking for something beyond what THE PLAN offers.</div> 
+
+		<div class="group pad4 margin1y keyline-top">
+			<div class="col6">
+				<ul>
+					<li>Switch hosts</li>
+					<li>Speed things up</li>
+					<li>Site hack recovery</li>
+				</ul>
+
+			</div>
+
+			<div class="col6">
+				<ul>
+					<li>Security checks</li>
+					<li>General Consultation</li>
+					<li>Muffin baking *</li>
+				</ul>
+
+
+			</div>
+		</div>
+
+		<div class="col12"><em>* Delivery fees apply.</em></div>
+
+		<div class="group col12">
+			<h3 class="callout-small">Any questions?</h3>
+			<a href="#" class="button blue">Get in touch <i class="icon-chevron-right"></i></a>
+		</div>
+
+
+	</div>
+</section>
+
+
+
+</div><!-- #content .site-content -->
+</div><!-- #primary .content-area -->
+
+<?php get_footer(); ?>
